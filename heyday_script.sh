30ea419613d5ad8fe7c25577ad5a9356a68ac660
twmckibben <106101740+twmckibben@users.noreply.github.com>
2022-10-02 22:27:49 -0500

    Add files via upload

---
+++ heyday_script.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+#Ritualistic Auburn Greeting
+echo "War Eagle! I'm Taylor, what's your name?"
+
+#user input
+read -p  "" name
+
+#friendly greeting
+echo "Hello, $name! Have a great day!"
